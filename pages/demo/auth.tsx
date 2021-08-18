06d9b771a3de3fc21004394e6d645d2f2f4f87da
frankfka <jiafrank98@gmail.com>
2021-08-17 17:09:29 -0700

    Remove demo pages

--- pages/demo/auth.tsx
+++
@@ -1,5 +0,0 @@
-import AuthenticationPage from '../../client/pages/demo/AuthenticationPage/AuthenticationPage';
-
-export default function Auth() {
-  return <AuthenticationPage />;
-}
--- pages/demo/index.tsx
+++
@@ -1,5 +0,0 @@
-import HomePage from '../../client/pages/demo/HomePage/HomePage';
-
-export default function Home() {
-  return <HomePage />;
-}
